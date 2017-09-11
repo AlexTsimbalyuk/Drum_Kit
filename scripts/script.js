ee63f2b8a3f632f4f3838dd08285763498290a23
Alexander Tsimbalyuk <a.tsimbalyuk1@gmail.com>
2017-09-11 06:53:10 +0300

    rework html-file, added styles

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+var v = document.getElementById("video");
+
+document.getElementById("playButton").onclick = function () {
+    v.play();
+};
+
+document.getElementById("pauseButton").onclick = function () {
+	v.pause();
+};
