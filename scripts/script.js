bf01c5c0bf9a0548cc4b83ffeb67916c0f166c61
Alexander Tsimbalyuk <a.tsimbalyuk1@gmail.com>
2017-09-12 02:12:25 +0300

    added video player and event handling for the drum kit

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,86 @@
-var v = document.getElementById("video");
+/* --- video player --- */
 
-document.getElementById("playButton").onclick = function () {
-    v.play();
+var video = document.getElementById("video");
+var btn = document.querySelector(".play-pause");
+
+btn.onclick = function () {
+	if ( video.currentTime >= 0 && btn.innerHTML == "PLAY" ) {
+		video.play();
+		btn.innerHTML = "PAUSE";
+	} 
+	else if ( video.currentTime > 0 && btn.innerHTML == "PAUSE" ) {
+		video.pause();
+		btn.innerHTML = "PLAY";
+	}
 };
 
-document.getElementById("pauseButton").onclick = function () {
-	v.pause();
-};
+video.ontimeupdate = function() {
+	if ( video.ended ) {
+		video.currentTime = 0;
+		btn.innerHTML = "PLAY";
+	}
+}
+
+/* --- drum kit --- */
+
+var drumPedal = document.querySelector(".bass-pedal-kick");
+var key;
+
+window.addEventListener("keydown", function( event ) {
+	key = document.querySelector('div[data-key="' + event.keyCode + '"]'); 
+	if ( !key ) {
+		return;
+	} else {
+		var sound = key.dataset.sound;
+		var audio = new Audio(); 
+		audio.src = sound;
+		audio.play();	
+		if ( key.className == "bass" ){
+			drumPedal.classList.add("active");
+			key.classList.add("active");
+		} else {
+			key.classList.add("active");
+		}
+	}
+});
+
+window.addEventListener("keyup", function( event ) {
+	if ( !key ) {
+		return;
+	} else {
+		key.classList.remove("active");
+		drumPedal.classList.remove("active");
+	}
+});
+
+/* --- audio player --- */
+/*
+function render() {
+	var opt = document.querySelectorAll("option");
+
+	for ( var i = 0; i < opt.length; i++ ){
+		if ( opt[i].selected ) {
+			song = opt[i].value;
+		}
+	}
+}
+
+var playAudio = document.querySelector(".play-audio");
+
+playAudio.onclick = function() {
+	
+	render();
+	var audio = new Audio();
+	audio.pause();
+	audio.src = song;
+	audio.play();
+}
+*/
+
+
+
+
+
+
+
+
